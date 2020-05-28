d3affa0a8be78958ec90b266ad9fe1849671f4f5
jarrodskahill <jarrodskahill@gmail.com>
2020-05-27 19:39:32 -0700

    added grid and some styling

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import GameCell from "./cell";
 
 export default class Grid extends Component {
   constructor(props) {
@@ -18,7 +19,19 @@ export default class Grid extends Component {
 
   handleRow(e) {}
 
-  gameGrid() {}
+  gameGrid() {
+    let newGrid = [];
+    let cells = [];
+
+    for (let x = 0; x < this.state.gridSize[0]; x++) {
+      for (let y = 0; y < this.state.gridSize[1]; y++) {
+        cells.push(<GameCell key={[x, y]} />);
+      }
+      newGrid.push(<div key={x}>{cells}</div>);
+      cells = [];
+    }
+    return newGrid;
+  }
 
   start() {}
 
@@ -27,20 +40,36 @@ export default class Grid extends Component {
   run() {}
   render() {
     return (
-      <div className="GridContainer">
-        <div>{this.gameGrid()}</div>
-        <label className="inputLabel">
-          Rows:
-          <input value={this.state.gridSize[0]} onChange={this.handleRow} />
-        </label>
-        <label className="inputLabel">
-          Colums:
-          <input value={this.state.gridSize[1]} onChange={this.handleCol} />
-        </label>
-        <h2>Generation:</h2>
-        <div className="buttonContainer">
-          <button onClick={this.start}>Start</button>
-          <button onClick={this.stop}>Stop</button>
+      <div>
+        <h2 className="title">Generation:</h2>
+        <div className="GridContainer">{this.gameGrid()}</div>
+        <div className="GridInputs">
+          <div className="labels">
+            <label className="inputLabel">
+              Rows:
+              <input
+                className="input"
+                value={this.state.gridSize[0]}
+                onChange={this.handleRow}
+              />
+            </label>
+            <label className="inputLabel">
+              Colums:
+              <input
+                className="input"
+                value={this.state.gridSize[1]}
+                onChange={this.handleCol}
+              />
+            </label>
+          </div>
+          <div className="buttonContainer">
+            <button className="buttons" onClick={this.start}>
+              Start
+            </button>
+            <button className="buttons" onClick={this.stop}>
+              Stop
+            </button>
+          </div>
         </div>
       </div>
     );
